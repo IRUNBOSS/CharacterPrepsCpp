b645d4ec323a08cd8fb5863f4a787f37939732a3
Nuri Onacak <nuri.onacak@hotmail.com>
2025-02-07 16:26:39 +0300

    Enemy AI added
    -Basic patrol  added
    -Enemy AI animation added
    -Enemy animation blueprint updated

--- Source/CharachterPrepsCpp/Private/Enemy.cpp
+++ Source/CharachterPrepsCpp/Private/Enemy.cpp
@@ -2,4 +2,83 @@
 
 
 #include "Enemy.h"
+#include "AIController.h"
+#include "Navigation/PathFollowingComponent.h"
+
+void AEnemy::BeginPlay()
+{
+	InitializeEnemy();
+	Super::BeginPlay();
+	
+	
+}
+
+void AEnemy::Tick(float DeltaTime)
+{
+	CheckPatrolTarget();
+	Super::Tick(DeltaTime);
+}
+
+bool AEnemy::InTargetRange(AActor* Target, double Radius)
+{
+	if (Target == nullptr) return false;
+	const double DistanceToTarget=(Target->GetActorLocation() - GetActorLocation()).Size();
+	return DistanceToTarget < Radius;
+}
+
+void AEnemy::CheckPatrolTarget()
+{
+	if (InTargetRange(PatrolTarget, PatrolRadius))
+	{
+		PatrolTarget = ChoosePatrolTarget();
+		GetWorldTimerManager().SetTimer(PatrolTimer, this, &AEnemy::PatrolTimerFinished,1.f);
+	}
+	
+}
+
+void AEnemy::PatrolTimerFinished()
+{
+	MoveToTarget(PatrolTarget);
+}
+
+AActor* AEnemy::ChoosePatrolTarget()
+{
+	TArray<AActor*> ValidTargets;
+	for (AActor* Target : PatrolTargets)
+	{
+		if (Target!=PatrolTarget)
+		{
+			ValidTargets.AddUnique(Target);
+		}
+	}
+
+	const int32 NumPatrolTargets = ValidTargets.Num();
+	if (NumPatrolTargets > 0)
+	{
+		const int32 TargetSelection=FMath::RandRange(0, NumPatrolTargets-1);
+		return ValidTargets[TargetSelection];
+	}
+	
+	return nullptr;
+}
+
+
+
+void AEnemy::InitializeEnemy()
+{
+	EnemyController= Cast<AAIController>(GetController());
+	MoveToTarget(PatrolTarget);
+	
+}
+
+void AEnemy::MoveToTarget(AActor* Target)
+{
+	FAIMoveRequest MoveRequest;
+	MoveRequest.SetGoalActor(Target);
+	MoveRequest.SetAcceptanceRadius(100.f);
+	EnemyController->MoveTo(MoveRequest);
+	
+}
+
+
 
--- Source/CharachterPrepsCpp/Public/Enemy.h
+++ Source/CharachterPrepsCpp/Public/Enemy.h
@@ -12,6 +12,31 @@
 UCLASS()
 class CHARACHTERPREPSCPP_API AEnemy : public ABaseCharacter
 {
+	
 	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	
+	class AAIController* EnemyController;
+	void InitializeEnemy();
+	void MoveToTarget(AActor* Target);
+	bool InTargetRange(AActor* Target, double Radius);
+	void CheckPatrolTarget();
+	AActor* ChoosePatrolTarget();
+	FTimerHandle PatrolTimer;
+	void PatrolTimerFinished();
+
+	UPROPERTY(EditAnywhere)
+	double PatrolRadius =200.f;
+	
+	UPROPERTY(EditInstanceOnly, Category="AI Navigation")
+	AActor* PatrolTarget;
+
+	UPROPERTY(EditInstanceOnly, Category="AI Navigation")
+	TArray<AActor*> PatrolTargets;
 	
 };
